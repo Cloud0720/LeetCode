5b8f38855a2e05feaa0ae9348e2243c6f44960c3
Cloud0720 <57808569+Cloud0720@users.noreply.github.com>
2024-02-02 18:22:10 +0900

    Time: 51 ms (64.54%), Space: 15.7 MB (80.48%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def countBits(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        ans = []
+        for i in range(n+1):
+            sum = bin(i).count('1')
+            ans.append(sum)
+        return ans
+
+        

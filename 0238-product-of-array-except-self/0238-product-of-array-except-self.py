854fc83d21a53cb43672ab5d0201b964317e4241
Cloud0720 <57808569+Cloud0720@users.noreply.github.com>
2024-02-16 18:16:43 +0900

    Time: 146 ms (99.38%), Space: 17.2 MB (99.47%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -4,16 +4,25 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        
-        length = len(nums)
-        products = [1] * length
-        for i in range(1, length):
-            products[i] = products[i-1] * nums[i-1]
+        total = 1
+        check = 0
+        answer = []
 
-        right = nums[-1]
-        for i in range(length-2, -1, -1):
-            products[i] *= right
-            right *= nums[i]
-        
-        return products
+        for i in nums:
+            if i != 0:
+               total *= i 
+            else:
+                check += 1
+
+        for i in nums:
+            if check != 0 and i!=0 or check > 1:
+                answer.append(0)
+            
+            elif i == 0:
+                answer.append(total)
+            else:
+                answer.append(total/i)
+
+        return answer
+
         

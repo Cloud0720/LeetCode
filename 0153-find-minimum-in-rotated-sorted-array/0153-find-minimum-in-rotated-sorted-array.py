3c06fbb8daef7137e8e39011e91f0baefefa4677
Cloud0720 <57808569+Cloud0720@users.noreply.github.com>
2024-02-20 21:01:27 +0900

    Time: 16 ms (94.01%), Space: 11.9 MB (63.13%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -6,13 +6,19 @@ class Solution(object):
         """
         if nums[0] <= nums[-1]:
             return nums[0]
+        
         l, r = 0, len(nums)-1
+        
         while l <= r:
+            
             m = (l + r) // 2
+            
             if nums[m] < nums[0] <= nums[m - 1]:
                 return nums[m]
+            
             elif nums[m] < nums[0]:
                 r = m - 1
+                
             else:
                 l = m + 1
         
